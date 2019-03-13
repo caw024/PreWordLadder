4ae0dd10785de83cdb9450c2c9159a253b23b969
Calvin Aw <caw@stuy.edu>
2019-03-12 20:25:17 -0400

    gr8

---
+++ makefile
@@ -0,0 +1,3 @@
+run: wordladder.py
+	python3 wordladder.py harry.txt answers.txt
+	cat answers.txt
--- wordladder.py
+++ wordladder.py
@@ -8,49 +8,55 @@ def Process(infile,outfile):
   f.close()
   
   #length of list
-  i = len(check[0])
+  length = len(check[0])
   
   #word list
-  dictall = open('dictall.txt','r')
-  wordlist = dictall.split('\n')
-  dictall.close()
+  #dictall = open('dictall.txt','r')
+  with open('dictall.txt') as h:
+    wordlist = [line.rstrip('\n') for line in h]
   
   mywordlist = []
   
   #get all words of same length
   for k in wordlist:
-    if len(k) == i:
+    if len(k) == length:
       mywordlist.append(k)
       
   dictionary = {}
   
   #checks per letter
-  for a in check:
-    k = 0
-    while k < i:
+  for word in check:
+    index = 0
+    if len(word) < 1:
+      break
+    dictionary[word] = []
+    while index < length:
       j = 97
-      m = list(a)
       #change one letter of m everytime
       while j < 123:
-        m[k] = chr(j)
+        m = list(word)
+        m[index] = chr(j)
         m = "".join(m)
         if m in mywordlist:
-          if a in dictionary:
-            val = dictionary[a]
-            val.append(m)
-            dictionary[a] = val
+          if m == word:
+            pass
           else:
-            dictionary[a] = m          
+            val = dictionary[word]
+            val.append(m)
+            dictionary[word] = val
+        j+=1
+      index+=1
       
   
   #g is what we will return
   g = open(outfile,'w')
   for k in dictionary:
-    g.write(k + ":" + str( len(dictionary[k])) )
+    g.write(k + "," + str( len(dictionary[k])) + "\n" )
   g.close()
     
             
 #$ ./fred.py A.txt(words of same length) B.txt
 def main():
   Process(sys.argv[1],sys.argv[2])
-  
+
+main()
